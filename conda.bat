12acff5dcc06587aeba8c7f316c6ae3335196319
jack <jgasper.wpp@gmail.com>
2018-11-16 07:27:28 -0800

    added and removed some files

---
+++ conda.bat
@@ -0,0 +1,6 @@
+conda create -n apprev python=3.7.1
+conda install -n apprev pandas
+conda install -n apprev nptdms
+conda install -n apprev acoustics
+conda install -n apprev jupyter
+conda install -n apprev ipython
